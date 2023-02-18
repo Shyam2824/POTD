f7ee0717d86b3963b3602708aaaa4652b7c03c8f
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-02-18 13:29:12 +0530

    leetcode

---
+++ Invert Binary Tree.java
@@ -0,0 +1,12 @@
+public class Invert Binary Tree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null)
+          return null;
+    
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        root.left = invertTree(right);
+        root.right = invertTree(left);
+        return root;
+      }
+}
