dbdf1b6569008cc9737f01d6be91026ba19cbc36
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-03-14 13:55:17 +0530

    gfg

---
+++ Number of list.java
@@ -0,0 +1,31 @@
+public class Number of list {
+    int maxCoins(int N, ArrayList<Integer> arr) {
+		//Write your code here
+		arr.add(0, 1);
+
+        arr.add(1);
+
+        int[][] dp = new int[N+2][N+2];
+
+        for (int len = 1; len <= N; len++) {
+
+            for (int i = 1; i <= N - len + 1; i++) {
+
+                int j = i + len - 1;
+
+                for (int k = i; k <= j; k++) {
+
+                    dp[i][j] = Math.max(dp[i][j], dp[i][k-1] + dp[k+1][j] + arr.get(i-1) * arr.get(k) * arr.get(j+1));
+
+                }
+
+            }
+
+        }
+
+        return dp[1][N];
+
+
+	}
+}
+}
