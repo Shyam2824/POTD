f2d66b501e3fdb6224a17cda2001f07d9c929ef9
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-04-01 22:13:34 +0530

    njbjb

---
+++ Make Array Elements Equal.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long minOperations(int N) {
+        // Code here
+          long n = (long)N;
+        return (n*n)/4;
+    }
+}class Solution {
+    public long minOperations(int N) {
+        // Code here
+          long n = (long)N;
+        return (n*n)/4;
+    }
+}
