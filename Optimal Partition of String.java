815beb10d9ed90f37730128c2101dce4bbcaffea
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-04-04 19:07:23 +0530

    leetcode problem

---
+++ Optimal Partition of String.java
@@ -0,0 +1,18 @@
+public class Optimal Partition of String {
+    public int partitionString(String s) {
+        int ans = 1;
+        int usedMask = 0;
+    
+        for (final char c : s.toCharArray()) {
+          final int i = c - 'a';
+          if ((usedMask >> i & 1) == 1) {
+            usedMask = 1 << i;
+            ++ans;
+          } else {
+            usedMask |= 1 << i;
+          }
+        }
+    
+        return ans;
+      }
+}
