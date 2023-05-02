4c0e30898ae8ff865a038783ba6ed038911f4da9
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-05-02 13:35:04 +0530

    gfgf

---
+++ Cake Distribution Problem.java
@@ -0,0 +1,29 @@
+public class Cake Distribution Problem {
+    int maxSweetness(int [] sweetness, int N, int K) {
+	    // Write your code here.
+	    int left = 1, right = 0;
+        for(int i=0; i<N; i++) {
+            right += sweetness[i];
+        }
+        int ans = 0;
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            int count = 0, sum = 0;
+            for(int i=0; i<N; i++) {
+                sum += sweetness[i];
+                if(sum >= mid) {
+                    count++;
+                    sum = 0;
+                }
+            }
+            if(count >= K+1) {
+                ans = mid;
+                left = mid+1;
+            }
+            else {
+                right = mid-1;
+            }
+        }
+        return ans;
+	}
+}
