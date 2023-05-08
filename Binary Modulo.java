d2723497e55bc37f9d58aa9647a9701284b607ea
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-05-08 19:12:11 +0530

    gfg

---
+++ Binary Modulo.java
@@ -0,0 +1,16 @@
+public class Binary Modulo {
+    int modulo(String s, int m) {
+		//Write your code here
+		  int len = s.length();
+        int ans = 0;
+        int x = 1;
+        for(int i = len- 1;i>=0; i--){
+            if(s.charAt(i) =='1' ){
+                ans = (ans +x)%m ;
+            }
+            x = x*2;
+            x %=m;
+         }
+         return ans;
+	} 
+}
