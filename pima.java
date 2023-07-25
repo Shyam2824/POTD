e870031a34b87d3e73221b94fefc017e8359dc2e
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-07-25 14:05:06 +0530

    leetcode

--- pima.java
+++ pima.java
@@ -1,3 +1,14 @@
 public class pima {
-    
+    int l = 0;
+    int r = arr.length - 1;
+
+    while (l < r) {
+      final int m = (l + r) / 2;
+      if (arr[m] >= arr[m + 1])
+        r = m;
+      else
+        l = m + 1;
+    }
+
+    return l;
 }
