89f6efc87da43c60f197ca22a5fd4dbd7d059d05
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-07-27 13:54:34 +0530

    leetcode

--- mrtc.java
+++ mrtc.java
@@ -1,3 +1,20 @@
+import java.util.Arrays;
+
 public class mrtc {
-    
+        long sum = Arrays.stream(batteries).asLongStream().sum();
+
+    Arrays.sort(batteries);
+
+    // Max battery is greater than the average, so it can last forever
+    // Reduce the problem from size n to size n - 1
+    int i = batteries.length - 1;
+    while (batteries[i] > sum / n) {
+      sum -= batteries[i--];
+      --n;
+    }
+
+    // If the max battery <= average running time,
+    // It won't be waste, and so do smaller batteries
+    return sum / n;
+
 }
