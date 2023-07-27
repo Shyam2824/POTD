57207df5c9547db320cd640ce282dd180cf8d622
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-07-27 13:48:01 +0530

    gfg

--- Heap Sort.java
+++ Heap Sort.java
@@ -1,3 +1,47 @@
 public class Heap Sort {
+
+       for(int i=n/2-1;i>=0;i--){
+            heapify(arr,n,i);
+        }
+        
+        //swap the element and heapify again
+        for(int i=n-1;i>=0;i--){
+            int temp=arr[0];
+            arr[0]=arr[i];
+            arr[i]=temp;
+            heapify(arr,i,0);
+        }
+    }
+ 
+    //Heapify function to maintain heap property.
+    void heapify(int arr[], int n, int i)
+    {
+        // Your code here
+        int largest=i;
+        int li=2*i+1;
+        int ri=2*i+2;
+        
+        if(li<n && arr[li]>arr[largest]){
+            largest=li;
+        }
+        if(ri<n && arr[ri]>arr[largest]){
+            largest=ri;
+        }
+        
+        if(largest!=i){
+            int temp=arr[i];
+            arr[i]=arr[largest];
+            arr[largest]=temp;
+            heapify(arr,n,largest);
+        }
+    }
+    
+    //Function to sort an array using Heap Sort.
+    public void heapSort(int arr[], int n)
+    {
+        //code here
+        Solution s=new Solution();
+        s.buildHeap(arr,n);
+
     
 }
