c9ae190037c573df91f08b2e3dae4e1d417cd97d
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-08-01 15:33:19 +0530

    gfg

--- DFS of Graph.java
+++ DFS of Graph.java
@@ -1,3 +1,23 @@
 public class DFS of Graph {
+    class Solution {
+    // Function to return a list containing the DFS traversal of the graph.
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        boolean[] visited = new boolean[V];
+        ArrayList<Integer> dfsTraversal = new ArrayList<>();
+        dfsUtil(0, adj, visited, dfsTraversal);
+        return dfsTraversal;
+    }
+
+    private void dfsUtil(int vertex, ArrayList<ArrayList<Integer>> adj, boolean[] visited, ArrayList<Integer> dfsTraversal) {
+        visited[vertex] = true;
+        dfsTraversal.add(vertex);
+
+        for (int neighbor : adj.get(vertex)) {
+            if (!visited[neighbor]) {
+                dfsUtil(neighbor, adj, visited, dfsTraversal);
+            }
+        }
+    }
+}
     
 }
