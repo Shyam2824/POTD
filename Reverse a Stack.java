5eaddb47f6e52ca4f9e6c176c53d12798c364ab6
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-08-04 11:53:34 +0530

    gfg

--- Reverse a Stack.java
+++ Reverse a Stack.java
@@ -1,3 +1,18 @@
-public class Reverse a Stack {
-    
+class Solution
+{ 
+    static void reverse(Stack<Integer> s)
+    {
+        // add your code here
+        Queue<Integer> que = new LinkedList<>();
+        while(!s.isEmpty()){
+            que.add(s.pop());
+        }
+        while(!que.isEmpty()){
+            s.push(que.remove());
+        }
+    }
 }
+
+ 
+
+
