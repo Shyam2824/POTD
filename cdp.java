a3096f676cead827b270c129fb9be8ce774b1996
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-08-05 17:00:20 +0530

    gfg

--- cdp.java
+++ cdp.java
@@ -1,3 +1,8 @@
-public class cdp {
-    
+class Solution{
+    public long findMinDiff (ArrayList<Integer> a, int n, int m){
+        Collections.sort(a);
+        int j = m-1,i = 0,min = Integer.MAX_VALUE;
+        while(j<n) min = Math.min(a.get(j++)-a.get(i++),min);
+        return min;
+    }
 }
