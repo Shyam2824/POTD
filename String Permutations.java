f3459fda87373873afabca7408430782f18100ec
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-08-06 10:53:31 +0530

    gfg

--- String Permutations.java
+++ String Permutations.java
@@ -1,22 +1,29 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class String Permutations {
-    public static void helper(ArrayList<String> ans,String s, String str) {
+    public static void helper(ArrayList<String> ans,String s, java.lang.String string) {
         if(s.length() == 0) {
-            ans.add(str);
+            ans.add(string);
             return;
         }
 
         for(int i=0; i<s.length(); i++) {
             char ch = s.charAt(i);
             String newStr = s.substring(0, i) + s.substring(i+1);
-            helper(ans, newStr, str+ch);
+            helper(ans, newStr, string+ch);
         }
+    }
+    private int length() {
+        return 0;
     }
     public ArrayList<String> permutation(String S)
     {
         //Your code here
         ArrayList<String> ans = new ArrayList<>();
         helper(ans,S,"");
-        Collections.sort(ans);
+        Collections.sort((List<T>) ans);
         return ans;
     }
     
