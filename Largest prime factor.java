90f2b049a6a4344270daaa0279deb32361f3f350
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-08-09 13:38:48 +0530

    gfg

--- Largest prime factor.java
+++ Largest prime factor.java
@@ -1,3 +1,18 @@
-public class Largest prime factor {
-    
+class Solution{
+    static long largestPrimeFactor(int N) {
+        // code here
+        int i=2;
+        int res = 2;
+        while(N!=1){
+            if(N%i==0){
+                N/=i;
+                res =Math.max(i,res);
+                i=2;
+                continue;
+            }
+            i++;
+        }
+        return res;
+    }
 }
+
