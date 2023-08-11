818d8d6b4cc9baea8d1cd26fe766acaaa187d36a
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-08-11 19:29:44 +0530

    leetcode

--- Coin Change II.java
+++ Coin Change II.java
@@ -1,3 +1,12 @@
-public class Coin Change II {
-    
+class Solution {
+  public int change(int amount, int[] coins) {
+    int[] dp = new int[amount + 1];
+    dp[0] = 1;
+
+    for (final int coin : coins)
+      for (int i = coin; i <= amount; ++i)
+        dp[i] += dp[i - coin];
+
+    return dp[amount];
+  }
 }
