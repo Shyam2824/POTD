6e7690b186b435ba5d92883cbc34b23de2f14795
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-08-14 16:29:28 +0530

    leetcode

--- kth.java
+++ kth.java
@@ -1,3 +1,19 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class kth {
-    
+    class Solution {
+  public int findKthLargest(int[] nums, int k) {
+    Queue<Integer> minHeap = new PriorityQueue<>();
+
+    for (final int num : nums) {
+      minHeap.offer(num);
+      while (minHeap.size() > k)
+        minHeap.poll();
+    }
+
+    return minHeap.peek();
+  }
+}
+
 }
