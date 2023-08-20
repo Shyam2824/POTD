e47e30b587e5e73f8098f76bf1f31f4071c41d8e
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-08-20 18:31:05 +0530

    gfg

--- noc.java
+++ noc.java
@@ -1,3 +1,14 @@
-public class noc {
-    
+class Solution {
+    int count(int[] arr, int n, int x) {
+        // code here
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i:arr){
+            if(i==x) map.put(i,map.getOrDefault(i,0)+1);
+        }
+        if(map.containsKey(x)){
+            return map.get(x);
+        }else{
+            return 0;
+        }
+    }
 }
