366bd867a64b32591ecd997b9b2cbd4e4fbf4a45
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-08-21 13:55:16 +0530

    leetcode

--- sp.java
+++ sp.java
@@ -1,3 +1,9 @@
 public class sp {
-    
+    class Solution {
+        public boolean repeatedSubstringPattern(String s) {
+          final String ss = s + s;
+          return ss.substring(1, ss.length() - 1).contains(s);
+        }
+      }
+        
 }
