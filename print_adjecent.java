2b6a3cd3de7fed011a419a9f3181136f1b01d3c1
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-09-05 13:46:37 +0530

    gfg

--- print_adjecent.java
+++ print_adjecent.java
@@ -1,3 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class print_adjecent {
-    
+    class Solution {
+    public List<List<Integer>> printGraph(int V, int edges[][]) {
+        // make the adjacency matrix
+        
+        List<List<Integer>>al=new ArrayList<>();
+        for(int i=0;i<V;i++){
+            al.add(new ArrayList<>());
+        }
+        for(int i=0;i<edges.length;i++){
+            al.get(edges[i][0]).add(edges[i][1]);
+            al.get(edges[i][1]).add(edges[i][0]);
+        }
+        
+        return al;
+    }
+}
 }
