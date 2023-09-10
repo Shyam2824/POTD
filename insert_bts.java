1b4028159c4b3a61e9cbb3236fd3f165b3a8d9f1
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-09-10 07:16:11 +0530

    insert a node in bts

--- insert_bts.java
+++ insert_bts.java
@@ -1,3 +1,22 @@
+import org.w3c.dom.Node;
+
 public class insert_bts {
-    
+    class Solution {
+    // Function to insert a node in a BST.
+    Node insert(Node root, int key) {
+        // your code here
+        if(root==null)
+        return new Node(key);
+        
+        if(root.data<key){
+            root.right= insert(root.right,key);
+        }
+        else if(root.data>key)
+        {
+        root.left=insert(root.left,key);
+       
+        }
+        return root;
+    }
+}
 }
