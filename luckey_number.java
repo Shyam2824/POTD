52303bd323ff41751b5392348846578cc6c7d8cd
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-09-11 09:33:48 +0530

    luckey number

--- luckey_number.java
+++ luckey_number.java
@@ -1,3 +1,15 @@
 public class luckey_number {
-    
+    public static boolean isLucky(int n)
+    {
+        // Your code here
+           int i=2;
+        while(n%i!=0){
+            if(n/i==0)
+            return true;
+            int r=n/i;
+            n=n-r;
+            i++;
+        }
+        return false;
+    }
 }
