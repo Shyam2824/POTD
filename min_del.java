8afa62ce8b35d55589ddcd49604d21b722b0264e
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-09-12 10:13:15 +0530

    minium deletion

---
+++ min_del.java
@@ -0,0 +1,26 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class min_del {
+    class Solution {
+  public int minDeletions(String s) {
+    int ans = 0;
+    int[] count = new int[26];
+    Set<Integer> usedFreq = new HashSet<>();
+
+    for (final char c : s.toCharArray())
+      ++count[c - 'a'];
+
+    for (int freq : count) {
+      while (freq > 0 && usedFreq.contains(freq)) {
+        --freq; // Delete ('a' + i).
+        ++ans;
+      }
+      usedFreq.add(freq);
+    }
+
+    return ans;
+  }
+}
+
+}
