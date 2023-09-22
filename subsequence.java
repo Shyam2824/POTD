3471aeefdb5d5c0efcb3c8c57c9c65716f2cd6b1
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-09-22 07:56:55 +0530

    is subsequence

--- subsequence.java
+++ subsequence.java
@@ -1,3 +1,16 @@
 public class subsequence {
-    
+    class Solution {
+        public boolean isSubsequence(String s, String t) {
+          if (s.isEmpty())
+            return true;
+      
+          int i = 0;
+          for (final char c : t.toCharArray())
+            if (s.charAt(i) == c && ++i == s.length())
+              return true;
+      
+          return false;
+        }
+      }
+      
 }
