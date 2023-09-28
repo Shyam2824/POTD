2bc03d3e10dbd45d80445f220462cde84088225e
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-09-28 14:43:21 +0530

    wave Array

--- wave_A.java
+++ wave_A.java
@@ -1,3 +1,12 @@
 public class wave_A {
-    
+    class Solution {
+        public static void convertToWave(int n, int[] arr) {
+            // code here
+            for(int i=1; i<n; i+=2) {
+                int temp=arr[i-1];
+                arr[i-1]=arr[i];
+                arr[i]=temp;
+            }
+        }
+    }
 }
