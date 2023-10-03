2584c4020fbf5822e3f39a71d14a6917579035d6
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-10-03 14:26:44 +0530

    Column name from a given column number

--- cccn.java
+++ cccn.java
@@ -1,3 +1,18 @@
 public class cccn {
-    
+    class Solution
+{
+    String colName (long n)
+    {
+        String s = "";
+        
+        while(n > 0){
+           long nu = (n - 1) % 26;
+            String a = Character.toString((char) ('A' + nu));
+            s = a + s;
+            n = (n - 1) / 26;
+
+        }
+        return s;
+    }
+}
 }
