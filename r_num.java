7b0af01372f2c31bc214d6be10b6ca54fc16caf0
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-10-04 15:00:04 +0530

    roman number to integer

--- r_num.java
+++ r_num.java
@@ -1,3 +1,32 @@
+import java.util.HashMap;
+
 public class r_num {
-    
+    class Solution {
+    // Finds decimal value of a given roman numeral
+    public int romanToDecimal(String str) {
+        // code here
+        HashMap<Character,Integer> h=new HashMap<Character,Integer>();
+        h.put('I',1);
+        h.put('V',5);
+        h.put('X',10);
+        h.put('L',50);
+        h.put('C',100);
+        h.put('D',500);
+        h.put('M',1000);
+        int c=0;
+        for(int i=1;i<str.length();i++){
+           Character pr=str.charAt(i-1); 
+           Character cu=str.charAt(i); 
+           if(h.get(pr)>=h.get(cu)){
+               c+=h.get(pr);
+           }
+           else{
+               c-=h.get(pr);
+           }
+        }
+        Character r=str.charAt(str.length()-1);
+        c+=h.get(r);
+        return c;
+    }
+}
 }
