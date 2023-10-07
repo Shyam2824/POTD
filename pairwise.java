9d818045ae33e4a3eb5b3ae7011981e2cda675f4
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-10-07 11:42:48 +0530

    gfg

--- pairwise.java
+++ pairwise.java
@@ -1,3 +1,31 @@
+import org.w3c.dom.Node;
+
 public class pairwise {
-    
+    public Node pairwiseSwap(Node head)
+    {
+        // code here
+        if(head==null || head.next==null)
+        {
+            return head;
+        }
+        Node p=head;
+        Node c=head.next;
+        head=c;
+        while(true){
+            Node next=c.next;
+            c.next=p;
+            if(next==null || next.next==null)
+            {
+                p.next=next;
+                break;
+                
+            }
+            p.next=next.next;
+            p=next;
+            c=p.next;
+            }
+            return head;
+        }
+        
+    }
 }
