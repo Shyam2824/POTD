9e2126d05a4d222773e53758e2f042715651dd92
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-10-08 14:01:02 +0530

    insert sort list

--- ist.java
+++ ist.java
@@ -1,3 +1,19 @@
+import org.w3c.dom.Node;
+
 public class ist {
-    
+    Node sortedInsert(Node head1, int key) {
+        // Add your code here.
+        Node ptr = head1;
+        Node p = new Node(key);
+        if(head1.data >= key){
+            p.next = head1;
+            return p;
+        }
+        while(ptr.next!=null && ptr.next.data<=key ){
+            ptr = ptr.next;
+        }
+        p.next= ptr.next;
+        ptr.next =p;
+        return head1;
+    }
 }
