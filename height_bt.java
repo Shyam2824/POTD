6d82df0d71dacd3f6c786922d60ad4a683034663
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-10-09 16:43:56 +0530

    height of binary tree

--- height_bt.java
+++ height_bt.java
@@ -1,3 +1,14 @@
+import org.w3c.dom.Node;
+
 public class height_bt {
-    
+      int height(Node node) 
+    {
+        // code here 
+        if(node==null){
+            return 0;
+        }
+        int l=1+height(node.left);
+        int r=1+height(node.right);
+        return Math.max(l,r);
+    }
 }
