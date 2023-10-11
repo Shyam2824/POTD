c19edb6b60bbfb62eb4880e029d6ab883e56ccc1
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-10-11 19:15:24 +0530

    check for balanced tree

--- check_balanced_tree.java
+++ check_balanced_tree.java
@@ -1,3 +1,31 @@
+import org.w3c.dom.Node;
+
 public class check_balanced_tree {
+    boolean isBalanced(Node root)
+    {
+    // Your code here
+        if(root == null) return true;
     
+        int leftHeight = getHeight(root.left);
+        int rightHeight = getHeight(root.right);
+        
+        int diff = Math.abs(leftHeight - rightHeight);
+        if(diff > 1) return false;
+        
+        
+        
+        return isBalanced(root.right) && isBalanced(root.left);
+        
+        
+    }
+    
+    public static int getHeight(Node root){
+        if(root == null){
+            return 0;
+        }
+        int leftHeight = getHeight(root.left);
+        int rightHeight = getHeight(root.right);
+        
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
 }
