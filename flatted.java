5dbd562df90574ccb956e3e6642792deea7cc923
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-10-20 21:13:31 +0530

    Flatten Nested List Iterator

--- flatted.java
+++ flatted.java
@@ -1,3 +1,33 @@
+import java.util.ArrayDeque;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Queue;
+
 public class flatted {
-    
+   public class NestedIterator implements Iterator<Integer> {
+  public NestedIterator(List<NestedInteger> nestedList) {
+    addInteger(nestedList);
+  }
+
+  @Override
+  public Integer next() {
+    return q.poll();
+  }
+
+  @Override
+  public boolean hasNext() {
+    return !q.isEmpty();
+  }
+
+  private Queue<Integer> q = new ArrayDeque<>();
+
+  private void addInteger(final List<NestedInteger> nestedList) {
+    for (final NestedInteger ni : nestedList)
+      if (ni.isInteger())
+        q.offer(ni.getInteger());
+      else
+        addInteger(ni.getList());
+  }
+}
+ 
 }
