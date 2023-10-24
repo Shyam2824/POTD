c8cf401f239c1c439b552e6f50751af2dfaaede2
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-10-24 16:11:42 +0530

    Find Largest Value in Each Tree Row

--- largest_value.java
+++ largest_value.java
@@ -1,3 +1,35 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
+
 public class largest_value {
-    
+    class Solution {
+  public List<Integer> largestValues(TreeNode root) {
+    if (root == null)
+      return new ArrayList<>();
+
+    List<Integer> ans = new ArrayList<>();
+    Queue<TreeNode> q = new ArrayDeque<>(Arrays.asList(root));
+
+    while (!q.isEmpty()) {
+      int max = Integer.MIN_VALUE;
+      for (int sz = q.size(); sz > 0; --sz) {
+        TreeNode node = q.poll();
+        max = Math.max(max, node.val);
+        if (node.left != null)
+          q.offer(node.left);
+        if (node.right != null)
+          q.offer(node.right);
+      }
+      ans.add(max);
+    }
+
+    return ans;
+  }
+}
+
 }
