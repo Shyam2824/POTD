d8e1f1935e58fe4823780e8ad40e9ce6a8910886
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-10-29 12:24:47 +0530

    poor pig

--- poor_pig.java
+++ poor_pig.java
@@ -1,3 +1,8 @@
 public class poor_pig {
-    
+    class Solution {
+        public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+          return (int) Math.ceil(Math.log(buckets) / Math.log(minutesToTest / minutesToDie + 1));
+        }
+      }
+      
 }
