8a579ce3e8414579c4f792d008bab3da6748003d
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-10-30 11:43:02 +0530

    Sum of XOR of all pairs'

--- sumofpair.java
+++ sumofpair.java
@@ -1,3 +1,18 @@
 public class sumofpair {
-    
+    class Solution{
+   
+        public long sumXOR (int arr[], int n) {
+            long val=1,ans=0;
+            
+            for(int i=0;i<31;i++){
+                int cnt=0;
+                for(int j=0;j<n;j++){
+                    cnt+= (arr[j]&val)>0?1:0;
+                }   
+                ans+= val*(n-cnt)*cnt;
+                val<<=1;
+            }
+            return ans;
+        }
+    }
 }
