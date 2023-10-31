121a4ac6f94828efd54f7753121a81f52e7cd725
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-10-31 12:34:53 +0530

    Move all zeroes to end of array

--- move_all_zero.java
+++ move_all_zero.java
@@ -1,3 +1,29 @@
 public class move_all_zero {
-    
+    class Solution {
+        void swap(int n1,int n2,int arr[]){  //Swap Function
+               int temp=arr[n1];
+               arr[n1]=arr[n2];
+               arr[n2]=temp;
+           }
+       void pushZerosToEnd(int[] arr, int n) { 
+           // code here
+           int nonZero=0;
+           int Zero=0;
+           if(arr.length==0||arr.length==1){ //Base Case
+               return;
+           }
+           while(nonZero<arr.length){
+               if(arr[nonZero]!=0){
+                   swap(nonZero,Zero,arr);
+                   nonZero++;
+                   Zero++;
+               }
+               else{
+                   nonZero++;
+               }
+           }
+          
+           
+       }
+   }
 }
