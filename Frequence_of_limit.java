211cd7b708e8bc636180470f896d8a22995a51c4
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-11-01 12:23:41 +0530

    Frequencies of Limited Range Array Elements

--- Frequence_of_limit.java
+++ Frequence_of_limit.java
@@ -1,3 +1,17 @@
+import java.util.HashMap;
+
 public class Frequence_of_limit {
-    
+    class Solution{
+    public static void frequencyCount(int arr[], int N, int P)
+    {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<N;i++){
+            if(arr[i]>=1 && arr[i]<=P)
+                map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+        }
+        for(int i=0;i<N;i++){
+            arr[i]=map.getOrDefault(i+1,0);
+        }
+    }
+}
 }
