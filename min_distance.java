e50b7189a1b90ba23c5f4c7135460fae08793410
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-11-02 21:20:43 +0530

    Minimum distance between two numbers

--- min_distance.java
+++ min_distance.java
@@ -1,3 +1,30 @@
 public class min_distance {
-    
+    int minDist(int a[], int n, int x, int y) {
+        // code here
+        int xIndex = -1;
+        int yIndex = -1;
+        int minDifference = n + 1;
+        
+        for(int i = 0; i < n; i++) {
+            if(a[i] == x) {
+                xIndex = i;
+            }
+            
+            if(a[i] == y) {
+                yIndex = i;
+            }
+            
+            if(xIndex != -1 && yIndex != -1) {
+                minDifference = Math.min(minDifference, Math.abs(xIndex - yIndex));
+            }
+        }
+        
+        if(minDifference == 0 || minDifference == n + 1) {
+            return -1;
+        }
+        
+        return minDifference;
+    }
+
+
 }
