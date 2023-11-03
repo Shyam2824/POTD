99ae21edd2ad0366614068f33d0ac5defdb305e3
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-11-03 21:03:30 +0530

    Build an Array With Stack Operations

--- build_array.java
+++ build_array.java
@@ -1,3 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class build_array {
-    
+    class Solution {
+    public List<String> buildArray(int[] target, int n) {
+        List<String> res = new ArrayList<>();
+        int current = 0;
+        int p = target.length;
+        for (int i = 1; i <= n; i++) {
+            if (current < p) {
+                if (target[current] == i) {
+                    res.add("Push");
+                    current++;
+                } else {
+                    res.add("Push");
+                    res.add("Pop");
+                }
+            } else {
+                return res;
+            }
+        }
+        return res;
+    }
+}
 }
