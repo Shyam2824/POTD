e4c01968a9f0e32fd7ded1e92b2d1a4cd2b33f39
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-11-03 20:59:10 +0530

    Pythagorean Triplet

--- triplet.java
+++ triplet.java
@@ -1,3 +1,23 @@
+import java.util.HashSet;
+
 public class triplet {
-    
+    class Solution {
+    boolean checkTriplet(int[] arr, int n) {
+        
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0;i<n;i++){
+            set.add(arr[i]*arr[i]);
+        }
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int c = arr[i]*arr[i]+arr[j]*arr[j];
+                if(set.contains(c)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
 }
