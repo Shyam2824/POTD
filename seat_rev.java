9c424dd9ed6dc1e7a501917c884b2568a277ae84
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-11-06 16:07:56 +0530

    seat reveresation

--- seat_rev.java
+++ seat_rev.java
@@ -1,3 +1,22 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class seat_rev {
-    
+    class SeatManager {
+  public SeatManager(int n) {}
+
+  public int reserve() {
+    if (minHeap.isEmpty())
+      return ++num;
+    return minHeap.poll();
+  }
+
+  public void unreserve(int seatNumber) {
+    minHeap.offer(seatNumber);
+  }
+
+  private Queue<Integer> minHeap = new PriorityQueue<>();
+  private int num = 0;
+}
+
 }
