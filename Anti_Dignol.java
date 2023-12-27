4cd24f32cd9eda3bab7347c41f49b83a5ebbb636
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-12-27 10:45:59 +0530

    Anti Diagonal Traversal of Matrix

--- Anti_Dignol.java
+++ Anti_Dignol.java
@@ -1,3 +1,25 @@
 public class Anti_Dignol {
-    
+    class Solution
+{
+    public int[] antiDiagonalPattern(int[][] matrix)
+    {
+        int n=matrix.length;
+        int ans[]=new int[n*n];
+        int r=0,c=0,x=0,z=0;
+        for(int k=0;k<(2*n)-1;k++){
+            if(k<n){
+                r=0;c=k;
+            }
+            else{
+                z++;
+                c=n-1;r=z;
+            }
+            while(r<n && c>=0){
+                ans[x++]=matrix[r][c];
+                r++;c--;
+            }
+        }
+        return ans;
+    }
+}
 }
