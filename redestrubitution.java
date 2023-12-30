044ba54467abfb79eec73840669027f342f44700
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-12-30 09:26:26 +0530

    redistribute-characters-to-make-all-strings-equal

---
+++ redestrubitution.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class redestrubitution {
+    class Solution {
+  public boolean makeEqual(String[] words) {
+    int[] count = new int[26];
+
+    for (final String word : words)
+      for (final char c : word.toCharArray())
+        ++count[c - 'a'];
+
+    return Arrays.stream(count).allMatch(c -> c % words.length == 0);
+  }
+}
+}
