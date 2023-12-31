8cfb7bc8dd8a97a10fbc850d5c0ccffb99c05127
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-12-31 09:50:34 +0530

    new year resolution

--- new_Year_R.java
+++ new_Year_R.java
@@ -1,3 +1,22 @@
 public class new_Year_R {
+    class Solution {
+        private static Boolean dp[][];
+        public static boolean isPossible(int N, int[] coins) {
+            dp = new Boolean[N+1][2025];
+            return dfs(coins, 0, 0);
+        }
+        private static boolean dfs(int[] arr, int i, int sum) {
+            if (sum != 0 && (sum % 20 == 0 || sum % 24 == 0 || sum == 2024)) return true;
+            if (i == arr.length) return false;
+            if (dp[i][sum] != null) return dp[i][sum];
     
+            // ignore/ not take the current value to our answer
+            boolean nonTake = dfs(arr, i+1, sum);
+    
+            // we take the current value to our answer
+            boolean take = dfs(arr, i+1, sum+arr[i]);
+    
+            return dp[i][sum] = nonTake || take;
+        }
+    }
 }
