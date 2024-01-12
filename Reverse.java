bf786f79564358441f759c4012f14675a7a73136
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2024-01-12 14:18:44 +0530

    reverse-first-k-elements-of-queue

--- Reverse.java
+++ Reverse.java
@@ -1,3 +1,23 @@
+import java.util.Queue;
+import java.util.Stack;
+
 public class Reverse {
-    
+    class GfG {
+    // Function to reverse first k elements of a queue.
+    public Queue<Integer> modifyQueue(Queue<Integer> q, int k) {
+        Stack<Integer> st= new Stack<>();
+        int n= q.size();
+        for(int i=0;i<k;i++){
+            st.push(q.remove());
+        }
+        while(!st.isEmpty()){
+            q.add(st.pop());
+        }
+        for(int i=0;i<n-k;i++){
+            st.push(q.remove());
+            q.add(st.pop());
+        }
+        return q;
+    }
+}
 }
