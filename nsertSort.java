4b4bde94990ef7e01c3e79ea56e293b572b56bff
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2024-01-13 12:14:30 +0530

    insert sort in single linklist

--- nsertSort.java
+++ nsertSort.java
@@ -1,3 +1,26 @@
+import org.w3c.dom.Node;
+
 public class nsertSort {
-    
+    class Solution
+{
+    public static Node insertionSort(Node head)
+    {
+        //code here
+        Node dummy = new Node(-1);
+        
+        while(head != null){
+            Node temp = dummy;
+            Node next = head.next;
+            
+            while(temp.next != null && temp.next.data < head.data){
+                temp = temp.next;
+            }
+            head.next = temp.next;
+            temp.next = head;
+            head = next;
+        }
+        
+        return dummy.next;
+    }
+}
 }
