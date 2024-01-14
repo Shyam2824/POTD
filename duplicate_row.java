f9798ab467ce6b9be51300d0eed8b6e2f9dd9282
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2024-01-14 16:33:03 +0530

    Find duplicate rows in a binary matrix

--- duplicate_row.java
+++ duplicate_row.java
@@ -1,3 +1,24 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 public class duplicate_row {
-    
+    class Solution
+{
+    public static ArrayList<Integer> repeatedRows(int matrix[][], int m, int n)
+    {
+        //code here
+        ArrayList<Integer> ans= new ArrayList<Integer>();
+        Set<String> set= new HashSet<>();
+        for(int i=0;i<m;i++){
+            StringBuilder curr=new StringBuilder();
+            for(int j=0;j<n;j++){
+                curr.append(matrix[i][j]+" ");
+            }
+            if(set.contains(curr.toString()))ans.add(i);
+            set.add(curr.toString());
+        }
+        return ans;
+    }
+}
 }
