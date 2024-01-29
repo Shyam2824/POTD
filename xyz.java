f1605dc56ec2660a6a37c3de4d94a06e99e7cf1e
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2024-01-29 12:28:17 +0530

    sort the array

---
+++ xyz.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class xyz {
+    public static void main(String[] args) {
+        int[] number={1,2,5,7,9,4,3,6};
+        Arrays.sort(number);
+        System.out.println(number);
+    }
+}
