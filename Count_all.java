0891a7d230d4a0192c906b64518b6f32d38baff4
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2024-02-15 07:17:06 +0530

    count all posible path

---
+++ Count_all.java
@@ -0,0 +1,16 @@
+public class Count_all {
+    public static void main(String[] args) {
+        for(int i=0;i<paths.length;i++)
+        {
+	        int count = 0;
+	        for(int j=0;j<paths.length;j++)
+	        {
+	            if(paths[i][j]==1)
+	                count++;
+	        }
+	        if(count%2==1) 
+	            return 0;
+	    }
+	    return 1;
+    }
+}
