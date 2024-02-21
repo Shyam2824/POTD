0277533003f370ec68bc68f157f9e7c6c7539203
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2024-02-21 13:03:59 +0530

    Bitwise AND of Numbers Range

---
+++ bitwiseand.java
@@ -0,0 +1,15 @@
+public class bitwiseand {
+    class Solution {
+        public int rangeBitwiseAnd(int m, int n) {
+          int shiftBits = 0;
+      
+          while (m != n) {
+            m >>= 1;
+            n >>= 1;
+            ++shiftBits;
+          }
+      
+          return m << shiftBits;
+        }
+      }
+}
